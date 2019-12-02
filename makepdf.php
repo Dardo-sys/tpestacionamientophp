381209632174bd2cc7cc7d21bb5e58fb79a9da5a
Your Name <you@example.com>
2019-12-02 13:47:49 -0300

    actualiz

--- makepdf.php
+++ makepdf.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
+//require_once __DIR__ . '/composer/autoload_real.php';
 
 //variables
   $patente = $_GET["patente"];
