3c654ff06f092b6ae65115916f3815310e550b2c
Your Name <you@example.com>
2019-12-02 14:01:48 -0300

    actualiz

--- makepdf.php
+++ makepdf.php
@@ -33,10 +33,10 @@ $data = '';
 $data .='<h1>Su Ticket</h1>';
 
 //agregar data
-$data .= '<strong>Vehiculo:</strong>' .$patente.'<br>';
-$data .= '<strong>Hora de ingreso:</strong>' .date("d-m-y H:i",$ingreso).'<br>';
-$data .= '<strong>Hora de salida:</strong>' .date("d-m-y H:i",$salida).'<br>';
-$data .= '<strong>Cobrar:</strong> $' .$cobrar.'<br>';
+$data .= '<strong>Vehiculo:</strong>' . $patente .'<br>';
+$data .= '<strong>Hora de ingreso:</strong>' . date("d-m-y H:i",$ingreso) .'<br>';
+$data .= '<strong>Hora de salida:</strong>' . date("d-m-y H:i",$salida) .'<br>';
+$data .= '<strong>Cobrar:</strong> $' . $cobrar .'<br>';
 
 //escribir pdf
 $mpdf->WriteHTML($data);
