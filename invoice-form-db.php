b05b56f9f20d5cc5a35a6589ff0d8fa6c36b3e2c
Your Name <you@example.com>
2019-12-03 03:37:06 -0300

    pdf segundo intento

--- invoice-form-db.php
+++ invoice-form-db.php
@@ -1,13 +1,26 @@
 <?php
 //db connection
-$con = mysqli_connect('mysql:host=remotemysql.com;dbname=RV6OjRGtny;charset=utf8', 'RV6OjRGtny', 'a7BUsFJ0gQ',);
+$con = mysqli_connect('mysql:host=remotemysql.com, dbname=RV6OjRGtny,charset=utf8', 'RV6OjRGtny', 'a7BUsFJ0gQ');
 mysqli_select_db($con,'RV6OjRGtny');
-/* devuelve el nombre de la base de datos actualmente seleccionada */
-if ($result = $mysqli->query("SELECT DATABASE()")) {
-    $row = $result->fetch_row();
-    printf("Default database is %s.\n", $row[0]);
-    $result->close();
+
+
+
+
+
+if (!$con) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($con) . PHP_EOL;
+
+mysqli_close($con);
+
+
+
 ?>
 
 <html>
